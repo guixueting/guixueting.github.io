3100c425e7bb7fb7e6a6f6bcd46ecf63e3147b55
guixueting <guixueting@smm.cn>
2023-06-07 16:46:08 +0800

    init

---
+++ themes/Butterfly/scripts/filters/random_cover.js
@@ -0,0 +1,45 @@
+/**
+ * Butterfly
+ * ramdom cover
+ */
+
+'use strict'
+
+hexo.extend.filter.register('before_post_render', function (data) {
+  const imgTestReg = /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/i
+  let randomCover
+  let coverVal = data.cover
+
+  // Add path to top_img and cover if post_asset_folder is enabled
+  if (hexo.config.post_asset_folder) {
+    const topImg = data.top_img
+    if (topImg && topImg.indexOf('/') === -1 && imgTestReg.test(topImg)) data.top_img = data.path + topImg
+    if (coverVal && coverVal.indexOf('/') === -1 && imgTestReg.test(coverVal)) data.cover = data.path + coverVal
+  }
+
+  const randomCoverFn = () => {
+    const theme = hexo.theme.config
+    if (!(theme.cover && theme.cover.default_cover)) return false
+    if (!Array.isArray(theme.cover.default_cover)) return theme.cover.default_cover
+    const num = Math.floor(Math.random() * theme.cover.default_cover.length)
+    return theme.cover.default_cover[num]
+  }
+
+  if (coverVal === false) return data
+
+  // If cover is not set, use random cover
+  if (!coverVal) {
+    randomCover = randomCoverFn()
+    data.cover = randomCover
+    coverVal = randomCover // update coverVal
+  }
+
+  if (coverVal) {
+    if (coverVal.indexOf('//') !== -1 || imgTestReg.test(coverVal)) {
+      data.cover_type = 'img'
+      return data
+    }
+  }
+
+  return data
+})
---
+++ themes/Butterfly/scripts/helpers/page.js
@@ -0,0 +1,102 @@
+/**
+ * Butterfly
+ * @example
+ *  page_description()
+ *  cloudTags(source, minfontsize, maxfontsize, limit)
+ */
+
+'use strict'
+
+const { stripHTML, escapeHTML, prettyUrls } = require('hexo-util')
+const crypto = require('crypto')
+
+hexo.extend.helper.register('page_description', function () {
+  const { config, page } = this
+  let description = page.description || page.content || page.title || config.description
+
+  if (description) {
+    description = escapeHTML(stripHTML(description).substring(0, 150)
+      .trim()
+    ).replace(/\n/g, ' ')
+    return description
+  }
+})
+
+hexo.extend.helper.register('cloudTags', function (options = {}) {
+  const env = this
+  let { source, minfontsize, maxfontsize, limit, unit, orderby, order } = options
+  unit = unit || 'px'
+
+  let result = ''
+  if (limit > 0) {
+    source = source.limit(limit)
+  }
+
+  const sizes = []
+  source.sort('length').forEach(tag => {
+    const { length } = tag
+    if (sizes.includes(length)) return
+    sizes.push(length)
+  })
+
+  const length = sizes.length - 1
+  source.sort(orderby, order).forEach(tag => {
+    const ratio = length ? sizes.indexOf(tag.length) / length : 0
+    const size = minfontsize + ((maxfontsize - minfontsize) * ratio)
+    let style = `font-size: ${parseFloat(size.toFixed(2))}${unit};`
+    const color = 'rgb(' + Math.floor(Math.random() * 201) + ', ' + Math.floor(Math.random() * 201) + ', ' + Math.floor(Math.random() * 201) + ')' // 0,0,0 -> 200,200,200
+    style += ` color: ${color}`
+    result += `<a href="${env.url_for(tag.path)}" style="${style}">${tag.name}</a>`
+  })
+  return result
+})
+
+hexo.extend.helper.register('urlNoIndex', function (url = null, trailingIndex = false, trailingHtml = false) {
+  return prettyUrls(url || this.url, { trailing_index: trailingIndex, trailing_html: trailingHtml })
+})
+
+hexo.extend.helper.register('md5', function (path) {
+  return crypto.createHash('md5').update(decodeURI(this.url_for(path))).digest('hex')
+})
+
+hexo.extend.helper.register('injectHtml', function (data) {
+  let result = ''
+  if (!data) return ''
+  for (let i = 0; i < data.length; i++) {
+    result += data[i]
+  }
+  return result
+})
+
+hexo.extend.helper.register('findArchivesTitle', function (page, menu, date) {
+  if (page.year) {
+    const dateStr = page.month ? `${page.year}-${page.month}` : `${page.year}`
+    const dateFormat = page.month ? hexo.theme.config.aside.card_archives.format : 'YYYY'
+    return date(dateStr, dateFormat)
+  }
+
+  const defaultTitle = this._p('page.archives')
+  if (!menu) return defaultTitle
+
+  const loop = (m) => {
+    for (const key in m) {
+      if (typeof m[key] === 'object') {
+        loop(m[key])
+      }
+
+      if (/\/archives\//.test(m[key])) {
+        return key
+      }
+    }
+  }
+
+  return loop(menu) || defaultTitle
+})
+
+hexo.extend.helper.register('isImgOrUrl', function (path) {
+  const imgTestReg = /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/i
+  if (path.indexOf('//') !== -1 || imgTestReg.test(path)) {
+    return true
+  }
+  return false
+})
